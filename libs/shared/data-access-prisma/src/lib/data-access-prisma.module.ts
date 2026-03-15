356b19c0b04c66d8b333ccd8696cb18fa9a69519
Thein Hein <thein.than.hein.th@gmail.com>
2026-03-14 22:29:32 -0500

    set up data-access-prisma

--- libs/shared/data-access-prisma/src/lib/data-access-prisma.module.ts
+++ libs/shared/data-access-prisma/src/lib/data-access-prisma.module.ts
@@ -1,10 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { DataAccessPrismaService } from './data-access-prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Global()
 @Module({
   controllers: [],
   providers: [DataAccessPrismaService],
   exports: [DataAccessPrismaService],
+  imports: [ConfigModule.forRoot()],
 })
 export class DataAccessPrismaModule {}
--- libs/shared/data-access-prisma/src/lib/data-access-prisma.service.ts
+++ libs/shared/data-access-prisma/src/lib/data-access-prisma.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class DataAccessPrismaService {}
+export class DataAccessPrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({ url: process.env['DATABASE_URL'] });
+    super({ adapter });
+  }
+}
