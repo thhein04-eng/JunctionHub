5e32587060fe33c0f90b8f2e30060518592ec2d8
thhein04-eng <th.hein04@gmail.com>
2026-03-15 17:48:29 -0500

    generate onboarding module

--- apps/organization_api/src/app/app.module.ts
+++ apps/organization_api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrganizationModule } from './organization/organization.module';
 import { DataAccessPrismaModule } from '@junction-hub/shared/data-access-prisma';
+import { OnboardingModule } from './onboarding/onboarding.module';
 
 @Module({
-  imports: [OrganizationModule, DataAccessPrismaModule],
+  imports: [OrganizationModule, DataAccessPrismaModule, OnboardingModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/organization_api/src/app/onboarding/onboarding.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('onboarding')
+export class OnboardingController {}
