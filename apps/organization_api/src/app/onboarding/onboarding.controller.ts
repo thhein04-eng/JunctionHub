cd40ff4b9537c7c5c705f0ee655b5d567d86baea
thhein04-eng <th.hein04@gmail.com>
2026-03-22 02:53:52 -0500

    add todo

--- apps/organization_api/src/app/onboarding/onboarding.controller.ts
+++ apps/organization_api/src/app/onboarding/onboarding.controller.ts
@@ -8,6 +8,16 @@ export class OnboardingController {
 
   @Post()
   start(@Body() dto: StartOnboardingDto) {
+    // TODO Create a new lib, named 'feature-organization-server'
+    // TODO Create a new service, named 'onboarding'
+    // TODO Create a new method, named 'onboard'
+    // TODO Create an org
+    // TODO Create a new user in Keycloak
+    // (call @junction-hub/data-access-auth-server#UserService#createUser)
+    // TODO Store the user in DB
+    // TODO Add the user to the org as an admin
+    // TODO Assign the user as admin in FGA
+    // TODO From this method, call that method
     return this.organizationService.createWithSeed({
       name: dto.name,
       industryTypeSlug: dto.industryTypeSlug,
